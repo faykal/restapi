a9086136b498120935b25f01dedbffc071b65f08
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-05-10 01:01:27 +0700

    Update gempa.js
    fix gempa

--- src/api/random/gempa.js
+++ src/api/random/gempa.js
@@ -5,7 +5,7 @@ app.get('/random/gempa', async (req, res) => {
         const { data } = await axios.get(`https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json`)
         res.status(200).json({
             status: true,
-            result: data.Infogempa
+            result: data.Infogempa.gempa
         });
     } catch (error) {
         res.status(500).send(`Error: ${error.message}`);
