c233223b1484722b168072b00bbf4dc4de992b23
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-05-10 18:31:49 +0700

    Update bard.js
    typo

--- src/api/ai/bard.js
+++ src/api/ai/bard.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
             if (!text) {
                 return res.status(400).json({ status: false, error: 'Text is required' });
             }
-                const fay = await axios.get(`https://api.kenshiro.cfd/api/ai/bard?text=${id}`)
+                const fay = await axios.get(`https://api.kenshiro.cfd/api/ai/bard?text=${text}`)
                 res.status(200).json({
                     status: true,
                     result: fay.data.data
