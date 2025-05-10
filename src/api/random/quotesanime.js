e9f6b8d07740de427813b6a8fe4741ecf0f8dcbe
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-05-10 18:32:29 +0700

    Update quotesanime.js
    typo kurang

--- src/api/random/quotesanime.js
+++ src/api/random/quotesanime.js
@@ -5,7 +5,7 @@ app.get('/random/quotesanime', async (req, res) => {
         const { data } = await axios.get(`https://api.kenshiro.cfd/api/random/quotesanime`)
         res.status(200).json({
             status: true,
-            result: data
+            result: data.data
         });
     } catch (error) {
         res.status(500).send(`Error: ${error.message}`);
