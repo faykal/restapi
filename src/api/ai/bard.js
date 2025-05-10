9520a413f80e1363893bc17f4c5e2e6cb257cdf4
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-05-10 18:21:23 +0700

    Add files via upload

---
+++ src/api/ai/bard.js
@@ -0,0 +1,18 @@
+const axios = require("axios")
+module.exports = function (app) {
+    app.get('/ai/bard', async (req, res) => {
+            try {
+            const { text } = req.query;
+            if (!text) {
+                return res.status(400).json({ status: false, error: 'Text is required' });
+            }
+                const fay = await axios.get(`https://api.kenshiro.cfd/api/ai/bard?text=${id}`)
+                res.status(200).json({
+                    status: true,
+                    result: fay.data.data
+                });
+            } catch (error) {
+                res.status(500).send(`Error: ${error.message}`);
+        }
+    });
+}
