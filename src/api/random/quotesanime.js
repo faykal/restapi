7723beb2cf6aec2f7bde68fcdc78bb11a86c4db4
Faykal Noxxy <64197103+faykal@users.noreply.github.com>
2025-05-10 18:22:02 +0700

    Add files via upload

---
+++ src/api/random/quotesanime.js
@@ -0,0 +1,14 @@
+const axios = require("axios")
+module.exports = function app (app) {
+app.get('/random/quotesanime', async (req, res) => {
+    try {
+        const { data } = await axios.get(`https://api.kenshiro.cfd/api/random/quotesanime`)
+        res.status(200).json({
+            status: true,
+            result: data
+        });
+    } catch (error) {
+        res.status(500).send(`Error: ${error.message}`);
+    }
+})
+};
